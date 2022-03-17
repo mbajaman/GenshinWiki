8b0e90ebb54b9f8cbc33f5c58ad9b683e31ce524
Kotailri <willc8810@gmail.com>
2022-03-16 20:51:02 -0700

    character list styling

--- index.js
+++ index.js
@@ -15,6 +15,10 @@ app.get('/public/html/character_list.html', (req, res) => {
   res.sendFile(path.join(__dirname, './character_list.html'))
 });
 
+app.get('/public/html/character_pages/albedo.html', (req, res) => {
+  res.sendFile(path.join(__dirname, './albedo.html'))
+});
+
 app.get('/public/html/navbar.html', (req, res) => {
   res.sendFile(path.join(__dirname, './navbar.html'))
 });
